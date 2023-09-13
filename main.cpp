0042e12828fd937b062c04b43b21da40100720cd
xGaujk <83506385+lamkkh9@users.noreply.github.com>
2023-09-13 11:15:39 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,39 +3,24 @@ using namespace std;
 
 int main() {
     float num[30];
-    float max, min, x = 0.0;
-    int i, n;
+    int n;
 
     cout << "So luong so can kiem tra: ";
     cin >> n;
 
-    for (i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         cout << "Nhap so thu " << i + 1 << ": ";
         cin >> num[i];
     }
 
-    cout << "Cac so da nhap: ";
-    for (i = 0; i < n; i++) {
-        cout << num[i] << " ";
-        x = x + num[i];
-    }
-    
-    cout << "\nTong day so da nhap: " << x << endl;
-    cout << "Trung binh cong day so da nhap: " << x / static_cast<float>(n) << endl;
-
-    max = num[0];
-    min = num[0];
-    for (i = 0; i < n; i++) {
-        if (num[i] > max) {
-            max = num[i];
-        }
-        if (num[i] < min) {
-            min = num[i];
+    for (int i = 0; i < n; i++) {
+        cout << "SO " << num[i] << " la ";
+        if (static_cast<int>(num[i]) % 2 == 0) {
+            cout << "Chan" << endl;
+        } else {
+            cout << "Le" << endl;
         }
     }
-
-    cout << "Max la: " << max << endl;
-    cout << "Min la: " << min << endl;
 
     return 0;
 }
