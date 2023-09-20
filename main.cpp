cc89e8f8294a0888728c4008400c5b592c575f58
xGaujk <83506385+lamkkh9@users.noreply.github.com>
2023-09-20 10:31:25 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,6 @@
 #include <iostream>
 using namespace std;
 
-void demmax(){
-	float num[30];
-    int n;
-
-    cout << "So luong so can kiem tra: ";
-    cin >> n;
-
-    for (int i = 0; i < n; i++) {
-        cout << "Nhap so thu " << i + 1 << ": ";
-        cin >> num[i];
-    }
-	double max=num[0];
-	int dem=1;
-    for (int i = 0; i < n; i++) {
-        if(num[i]>max){
-        	max=num[i];
-        	dem=1;
-		}else if(num[i]==max){
-			dem++;
-		}
-    }
-    cout<<"so lon nhat la :"<<max;
-    cout<<"co "<<dem<<"lon nhat";
-}
-
 int main() {
     float num[30];
     int n;
@@ -38,17 +13,18 @@ int main() {
         cin >> num[i];
     }
     
-    for (int i = 0; i < n-1; i++) {
+    for (int i = 0; i <= n-2; i++) {
     	for (int j = i+1; j < n; j++) {
-	        if(num[i]<num[j]){
-	        	double tg = num[i];
-	        	num[i]=num[j];
-	        	num[j]=tg;
+        if(num[i]>=num[j]){
+        	double tg = num[i];
+        	num[i]=num[j];
+        	num[j]=tg;
 			}
 		}
     }
+    cout<<"Day so sau xap xep la :";
     for (int i = 0; i < n; i++) {
-        cout << num[i]<<" ";
+        cout <<num[i]<<" ";
     }
     return 0;
 }
