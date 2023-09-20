a52d2f954de729bd6bc51f6b71838e689e70bb54
xGaujk <83506385+lamkkh9@users.noreply.github.com>
2023-09-20 10:55:42 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,29 +2,48 @@
 using namespace std;
 
 int main() {
-    float num[30];
-    int n;
-
-    cout << "So luong so can kiem tra: ";
-    cin >> n;
-
-    for (int i = 0; i < n; i++) {
-        cout << "Nhap so thu " << i + 1 << ": ";
+    int main() {
+	int n;
+	float num[100];
+    cout <<"Co bao nhieu sinh vien trong lop :";
+    cin>>n;
+    for(int i=0;i<n;i++){
+    	cout << "Nhap diem cua sinh vien so  " << i + 1 << ": ";
         cin >> num[i];
-    }
-    
-    for (int i = 0; i <= n-2; i++) {
-    	for (int j = i+1; j < n; j++) {
-        if(num[i]>=num[j]){
-        	double tg = num[i];
-        	num[i]=num[j];
-        	num[j]=tg;
-			}
+	}
+	double tb = 0;
+	for(int i=0;i<n;i++){
+		tb = tb+num[i];
+	}
+	tb= tb/n;
+	int ltb=0;
+	int svt =0;
+	for(int i=0;i<n;i++){
+		if(num[i]<5){
+			svt++;
+		}
+		if(num[i]>tb){
+			ltb++;
+		}
+	}
+	double max=num[0];
+	int dem=1;
+    for (int i = 0; i < n; i++) {
+        if(num[i]>max){
+        	max=num[i];
+        	dem=1;
+		}else if(num[i]==max){
+			dem++;
 		}
     }
-    cout<<"Day so sau xap xep la :";
-    for (int i = 0; i < n; i++) {
-        cout <<num[i]<<" ";
-    }
+	
+	cout<<"Diem trung binh lop :"<<tb<<endl;
+	cout<<"So sinh vien tren diem trung binh lop :"<<ltb<<endl;
+	cout<<"so hoc sinh duoi 5 diem :"<<svt<<endl;
+	cout<<"Diem cao nhat la :"<<max<<endl;
+	cout<<"So nguoi co diem cao nhat la :"<<dem<<endl;
+
+	
     return 0;
+}
 }
