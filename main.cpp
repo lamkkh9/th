caabd84e8fa7e45b9b3e5a0a5b6d9c2d9ab85304
xGaujk <83506385+lamkkh9@users.noreply.github.com>
2023-09-20 11:15:57 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,60 +1,6 @@
 #include <iostream>
 using namespace std;
 
-void demmax(){
-	float num[30];
-    int n;
-
-    cout << "So luong so can kiem tra: ";
-    cin >> n;
-
-    for (int i = 0; i < n; i++) {
-        cout << "Nhap so thu " << i + 1 << ": ";
-        cin >> num[i];
-    }
-	double max=num[0];
-	int dem=1;
-    for (int i = 0; i < n; i++) {
-        if(num[i]>max){
-        	max=num[i];
-        	dem=1;
-		}else if(num[i]==max){
-			dem++;
-		}
-    }
-    cout<<"so lon nhat la :"<<max;
-    cout<<"co "<<dem<<"lon nhat";
-    
-}
-
-void xapxep(){
-	float num[30];
-    int n;
-
-    cout << "So luong so can kiem tra: ";
-    cin >> n;
-
-    for (int i = 0; i < n; i++) {
-        cout << "Nhap so thu " << i + 1 << ": ";
-        cin >> num[i];
-    }
-    
-    for (int i = 0; i <= n-2; i++) {
-    	for (int j = i+1; j < n; j++) {
-        if(num[i]>=num[j]){
-        	double tg = num[i];
-        	num[i]=num[j];
-        	num[j]=tg;
-			}
-		}
-    }
-    cout<<"Day so sau xap xep la :";
-    for (int i = 0; i < n; i++) {
-        cout <<num[i]<<" ";
-    }
-}
-
-
 int main() {
 	int m,dem,ltb,svt;
 	float num[100];
