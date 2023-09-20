5812400a4e7b5817bbd02927d28cb13031952586
xGaujk <83506385+lamkkh9@users.noreply.github.com>
2023-09-20 10:14:34 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,17 +12,18 @@ int main() {
         cout << "Nhap so thu " << i + 1 << ": ";
         cin >> num[i];
     }
-	double max=num[0];
-	int dem=1;
-    for (int i = 0; i < n; i++) {
-        if(num[i]>max){
-        	max=num[i];
-        	dem=1;
-		}else if(num[i]==max){
-			dem++;
+    
+    for (int i = 0; i < n-1; i++) {
+    	for (int j = i+1; j < n; j++) {
+	        if(num[i]>num[j]){
+	        	double tg = num[i];
+	        	num[i]=num[j];
+	        	num[j]=tg;
+			}
 		}
     }
-    cout<<"co "<<dem<<"lon nhat";
-
+    for (int i = 0; i < n; i++) {
+        cout << num[i]<<" ";
+    }
     return 0;
 }
