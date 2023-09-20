06dffc54ccc4b6f41f56cc6bc07bbf9ce8d665b7
xGaujk <83506385+lamkkh9@users.noreply.github.com>
2023-09-20 11:15:03 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int main() {
-	int n;
-	float num[100];
-    cout <<"Co bao nhieu sinh vien trong lop :";
-    cin>>n;
-    for(int i=0;i<n;i++){
-    	cout << "Nhap diem cua sinh vien so  " << i + 1 << ": ";
+void demmax(){
+	float num[30];
+    int n;
+
+    cout << "So luong so can kiem tra: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Nhap so thu " << i + 1 << ": ";
         cin >> num[i];
-	}
-	double tb = 0;
-	for(int i=0;i<n;i++){
-		tb = tb+num[i];
-	}
-	tb= tb/n;
-	int ltb=0;
-	int svt =0;
-	for(int i=0;i<n;i++){
-		if(num[i]<5){
-			svt++;
-		}
-		if(num[i]>tb){
-			ltb++;
-		}
-	}
+    }
 	double max=num[0];
 	int dem=1;
     for (int i = 0; i < n; i++) {
@@ -36,14 +22,98 @@ int main() {
 			dem++;
 		}
     }
-	
-	cout<<"Diem trung binh lop :"<<tb<<endl;
-	cout<<"So sinh vien tren diem trung binh lop :"<<ltb<<endl;
-	cout<<"so hoc sinh duoi 5 diem :"<<svt<<endl;
-	cout<<"Diem cao nhat la :"<<max<<endl;
-	cout<<"So nguoi co diem cao nhat la :"<<dem<<endl;
+    cout<<"so lon nhat la :"<<max;
+    cout<<"co "<<dem<<"lon nhat";
+    
+}
 
+void xapxep(){
+	float num[30];
+    int n;
+
+    cout << "So luong so can kiem tra: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Nhap so thu " << i + 1 << ": ";
+        cin >> num[i];
+    }
+    
+    for (int i = 0; i <= n-2; i++) {
+    	for (int j = i+1; j < n; j++) {
+        if(num[i]>=num[j]){
+        	double tg = num[i];
+        	num[i]=num[j];
+        	num[j]=tg;
+			}
+		}
+    }
+    cout<<"Day so sau xap xep la :";
+    for (int i = 0; i < n; i++) {
+        cout <<num[i]<<" ";
+    }
+}
+
+
+int main() {
+	int m,dem,ltb,svt;
+	float num[100];
+	// nhap so sinh vien trong lop
+    cout <<"Co bao nhieu sinh vien trong lop :";
+    cin>>m;
+    
+    // nhap diem cua tung sinh vien
+    for(int i=0;i<m;i++){
+    	cout << "Nhap diem hoa cua sinh vien so  " << i + 1 << ": ";
+        cin >> num[i];
+	}
+	
+	// tinh diem trung binh cua lop
+	double tb = 0;
+	for(int i=0;i<m;i++){
+		tb = tb+num[i];
+	}
+	tb= tb/m;
+	
+	// kiem tra co bao nhieu sinh vien co diem duoi 5 va tren trung binh cua lop va tim diem cao nhat
+	double max=num[0];
+	dem=1;
+	ltb=0;
+	svt =0;
+	for(int i=0;i<m;i++){
+		if(num[i]<5){
+			svt++;
+		}
+		if(num[i]>tb){
+			ltb++;
+		}
+		if(num[i]>max){
+        	max=num[i];
+        	dem=1;
+		}
+		for (int j = i+1; j < m; j++) {
+	        if(num[i]>=num[j]){
+	        	double tg = num[i];
+	        	num[i]=num[j];
+	        	num[j]=tg;
+				}
+		}
+		
+	}
+	// 
+	
+	
+
+	cout<<"So sinh vien tren diem trung binh lop :"<<ltb<<endl;
+	
+	cout<<"so hoc sinh truot mon hoa :"<<svt<<endl;
+	
+	cout<<"Diem hoa cao nhat lop la :"<<max<<endl;
+
+	cout<<"Day so sau xap xep la :";
+    for (int i = 0; i < m; i++) {
+        cout <<num[i]<<" ";
+    }
 	
     return 0;
 }
-
