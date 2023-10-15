a769c61ed9d16c23e17fd583904c6d8f4545af23
xGaujk <83506385+lamkkh9@users.noreply.github.com>
2023-10-16 00:54:46 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,122 +1,89 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
 
-void demmax(){
-	float num[30];
-    int n;
-
-    cout << "So luong so can kiem tra: ";
-    cin >> n;
-
-    for (int i = 0; i < n; i++) {
-        cout << "Nhap so thu " << i + 1 << ": ";
-        cin >> num[i];
-    }
-	double max=num[0];
-	int dem=1;
-    for (int i = 0; i < n; i++) {
-        if(num[i]>max){
-        	max=num[i];
-        	dem=1;
-		}else if(num[i]==max){
-			dem++;
-		}
-    }
-    cout<<"so lon nhat la :"<<max;
-    cout<<"co "<<dem<<"lon nhat";
-    
-}
-
-void xapxep(){
-	float num[30];
-    int n;
-
-    cout << "So luong so can kiem tra: ";
-    cin >> n;
-
-    for (int i = 0; i < n; i++) {
-        cout << "Nhap so thu " << i + 1 << ": ";
-        cin >> num[i];
-    }
-    
-    for (int i = 0; i <= n-2; i++) {
-    	for (int j = i+1; j < n; j++) {
-        if(num[i]>=num[j]){
-        	double tg = num[i];
-        	num[i]=num[j];
-        	num[j]=tg;
-			}
-		}
-    }
-    cout<<"Day so sau xap xep la :";
-    for (int i = 0; i < n; i++) {
-        cout <<num[i]<<" ";
-    }
-}
-
+// Định nghĩa cấu trúc SinhVien
+struct SinhVien {
+    string ten;
+    int diem;
+};
 
 int main() {
-	int m,dem,ltb,svt;
-	float num[100];
-	// nhap so sinh vien trong lop
-    cout <<"Co bao nhieu sinh vien trong lop :";
-    cin>>m;
-    
-    // nhap diem cua tung sinh vien
-    for(int i=0;i<m;i++){
-    	cout << "Nhap diem hoa cua sinh vien so  " << i + 1 << ": ";
-        cin >> num[i];
-        if(num[i]<0 or num[i]>10){
-        	cout << "Nhap lai diem hoa cua sinh vien so  " << i + 1 << ": ";
-        cin >> num[i];
-		}
-	}
-	
-	// tinh diem trung binh cua lop
-	double tb = 0;
-	for(int i=0;i<m;i++){
-		tb = tb+num[i];
-	}
-	tb= tb/m;
-	
-	// kiem tra co bao nhieu sinh vien co diem duoi 5 va tren trung binh cua lop va tim diem cao nhat
-	double max=num[0];
-	dem=1;
-	ltb=0;
-	svt =0;
-	for(int i=0;i<m;i++){
-		if(num[i] < 5){
-			svt = svt+1;
-		}
-		if(num[i]>tb){
-			ltb++;
-		}
-		if(num[i]>max){
-        	max=num[i];
-        	dem=1;
-		}
-	}
-	for (int i = 0; i <= m-2; i++) {
-    	for (int j = i+1; j < m; j++) {
-        if(num[i]>=num[j]){
-        	double tg = num[i];
-        	num[i]=num[j];
-        	num[j]=tg;
-			}
-		}
-    }
-	
-	
-	cout<<"So sinh vien tren diem trung binh lop :"<<ltb<<endl;
-	
-	cout<<"so hoc sinh truot mon hoa :"<<svt<<endl;
-	
-	cout<<"Diem hoa cao nhat lop la :"<<max<<endl;
+    int m;
+    cout << "Co bao nhieu sinh vien trong lop: ";
+    cin >> m;
 
-	cout<<"Diem hoa sau khi xap xep la :";
+    // Tạo mảng chứa thông tin của từng sinh viên
+    vector<SinhVien> danhSachSinhVien(m);
+    // Nhập thông tin của từng sinh viên
     for (int i = 0; i < m; i++) {
-        cout <<num[i]<<" ";
+        cout << "Nhap ten sinh vien thu " << i + 1 << ": ";
+        cin >> danhSachSinhVien[i].ten;
+        cout << "Nhap diem hoa cua sinh vien thu " << i + 1 << ": ";
+        cin >> danhSachSinhVien[i].diem;
+
     }
-	
+    for (int i = 0; i < m; i++) {
+        switch (danhSachSinhVien[i].diem) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                cout << "Ten: " << danhSachSinhVien[i].ten << "- Diem: " << danhSachSinhVien[i].diem << " (Truot)" << endl;
+
+            default:
+          cout << "Ten: " << danhSachSinhVien[i].ten << "- Diem: " << danhSachSinhVien[i].diem << " (DO)" << endl;
+
+        }
+}
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int dong, cot;
+
+
+    cout << "Nhap so dong: ";
+    cin >> dong;
+    cout << "Nhap so cot: ";
+    cin >> cot;
+
+
+    int mang[100][100];
+
+
+    for (int i = 0; i < dong; i++) {
+        for (int j = 0; j < cot; j++) {
+            cout << "Nhap gia tri tai vi tri [" << i << "][" << j << "]: ";
+            cin >> mang[i][j];
+        }
+    }
+
+
+    int tong = 0;
+    for (int i = 0; i < dong; i++) {
+        for (int j = 0; j < cot; j++) {
+            tong += mang[i][j];
+        }
+    }
+
+    cout << "Tong cua mang 2 chieu la: " << tong << endl;
+
     return 0;
 }
