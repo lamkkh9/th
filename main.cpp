65ea18d8157082dbee51ed4df9ca9ee219e81417
xGaujk <83506385+lamkkh9@users.noreply.github.com>
2023-11-22 11:53:45 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,85 +1,58 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-const int MAX_SINHVIEN = 50;
-const int MAX_MONHOC = 50;
-const int MAX_DIEM = 50;
-
-struct SinhVien {
-    string ten;
-    string ten_mon;
-    float diem[MAX_MONHOC][MAX_DIEM];
-    float tb;
+struct Danh_Sach {
+    double so_a;
+    double so_b;
+    double hieu;
+    double tong;
+    double nhan;
 };
 
-int main() {
-    int n;
+double Tong(double a, double b) {
+    return a + b;
+}
 
-    cout << "Nhap so sinh vien: ";
-    cin >> n;
-    cout << endl;
+double Hieu(double a, double b) {
+    return a - b;
+}
 
-    SinhVien sv[MAX_SINHVIEN];
+double Nhan(double a, double b) {
+    return a * b;
+}
 
-    int m, l;
+void hien_thi (){
+	for (int j = 0; j < i; j++) {
+        cout << "Cap so thu " << j + 1 << " :" << endl;
+        cout << ds[j].so_a << " + " << ds[j].so_b << " = " << ds[j].tong << endl;
+        cout << ds[j].so_a << " - " << ds[j].so_b << " = " << ds[j].hieu << endl;
+        cout << ds[j].so_a << " x " << ds[j].so_b << " = " << ds[j].nhan << endl;
+    }
+}
+int main()
+{	
+    Danh_Sach ds[1000];
+    int i = 0;
 
-    cout << "Nhap so mon hoc: ";
-    cin >> m;
-
-    cout << "Nhap so dau diem cua tung mon: ";
-    cin >> l;
-
-
-    for (int i = 0; i < n; i++) {
-        cout << "-Nhap ten sinh vien thu " << i + 1 << ": ";
-        cin.ignore();
-        getline(cin, sv[i].ten);
-
-        for (int j = 0; j < m; j++) {
-            cout << " -Nhap ten mon thu " << j + 1 << ": ";
-            cin.ignore();
-            getline(cin, sv[i].ten_mon);
-            for (int k = 0; k < l; k++) {
-                cout << "  -Dau dien thu "<<k+1<<" cua mon "<<sv[i].ten_mon<<" : ";
-                cin >> sv[i].diem[j][k];
-            }
-        }
+    while (true) {
+        cout << "Nhap gia tri cua a: ";
+        cin >> ds[i].so_a;
+        if (!(ds[i].so_a) == false) {
+		
+	        cout << "Nhap gia tri cua b: ";
+	        cin >> ds[i].so_b;
+	
+	        ds[i].tong = Tong(ds[i].so_a, ds[i].so_b);
+	        ds[i].hieu = Hieu(ds[i].so_a, ds[i].so_b);
+	        ds[i].nhan = Nhan(ds[i].so_a, ds[i].so_b);
+	
+	        i++;
+		}else{
+			break;
+		}
     }
 
-    float tong;
-    for (int i = 0; i < n; i++) {
-        tong = 0;
-        for (int j = 0; j < m; j++) {
-            for (int k = 0; k < l; k++) {
-                tong += sv[i].diem[j][k];
-            }
-        }
-        sv[i].tb = tong / ((float)(l * m));
-    }
-
-
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (sv[i].tb < sv[j].tb) {
-                swap(sv[i], sv[j]);
-            }
-        }
-    }
-
-    cout << "Danh sach sau khi sap xep: " << endl;
-    for (int i = 0; i < n; i++) {
-        cout << "- STT: " << i+1 << endl;
-        cout << "- Ten sinh vien: " << sv[i].ten << endl;
-        for (int j = 0; j < m; j++) {
-            cout << "- Diem mon " << sv[i].ten_mon << " cua sinh vien nay la: " << endl;
-            for (int k = 0; k < l; k++) {
-                cout << "\t+ Diem thu "<<k+1<<" : " << sv[i].diem[j][k] << endl;
-            }
-        }
-        cout << "Diem trung binh: " << sv[i].tb << endl;
-    }
-    cout << endl;
+    
 
     return 0;
 }
